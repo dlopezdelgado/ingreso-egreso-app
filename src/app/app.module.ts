5ac2dbbc6c4caa1b987e1412ad3979889e02d550
Daniel Lopez <dlopezdelgado@gmail.com>
2020-02-28 21:52:06 -0500

    Seccion 8 Completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
+// NGRX
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducer';
+
 // Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -22,6 +27,8 @@ import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -42,7 +49,13 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    StoreModule.forRoot(appReducers),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.reducer.ts
@@ -0,0 +1,19 @@
+// Permite fusionar varios reducers en uno solo
+import { ActionReducerMap } from '@ngrx/store';
+
+
+
+// Reducers
+import * as fromUI from './shared/ui.reducer';
+import * as fromAuth from './auth/auth.reducer';
+
+export interface AppState {
+    ui: fromUI.State;
+    auth: fromAuth.AuthState;
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+    ui: fromUI.uiReducer,
+    auth: fromAuth.authReducer
+};
+
